bea16f3298dae4731e9abf86a9627a61a9f27e7f
AlanPan0131 <panalan0131@gmail.com>
2022-09-27 14:12:14 +0800

    Update headerParser.py

--- code/module/headerParser.py
+++ code/module/headerParser.py
@@ -2,15 +2,19 @@ def parsestr(reply):
     line = reply.split("\r\n")
     lastKey = ''
     response = {}
+    body = ''
     isInHeader = True
     for i in range(1,len(line)-1):
-        if line[i] == "":
-            isInHeader = False
-        elif line[i][0] != '\t':
-            key = line[i].split(': ')[0]
-            value = line[i].split(key+': ')[1]
-            response[key] = value
-            lastKey = key
+        if isInHeader:
+            if line[i] == "":
+                isInHeader = False
+            elif line[i][0] != '\t':
+                key = line[i].split(': ')[0]
+                value = line[i].split(key+': ')[1]
+                response[key] = value
+                lastKey = key
+            else:
+                response[lastKey] += line[i]
         else:
-            response[lastKey] += line[i]
-    return response
+            body += line[i] + '\r\n'
+    return response, body
