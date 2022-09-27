3d76aaf74a5d97da7e7a1cbc07cf661982cea5f1
AlanPan0131 <panalan0131@gmail.com>
2022-09-27 14:25:41 +0800

    Update index.py

--- code/index.py
+++ code/index.py
@@ -122,8 +122,7 @@ def main():
 #    if(len(sys.argv) < 2):
 #        print("Usage: python3 pop3client.py ServerIP")
 #        return
-    baseLoginWindow = tk.createTk()
-    tk.loginInit(baseLoginWindow, start)
+    baseLoginWindow = tk.loginWindow(start)
     #window = tk.createToplevel()
     baseLoginWindow.mainloop()
     return 
